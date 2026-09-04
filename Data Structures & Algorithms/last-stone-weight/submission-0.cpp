818461a6f7fa2ba7e4cddc2fb95d1809ba43ca3c
tomarkalpana <tomarkalpana120@gmail.com>
2026-09-04 16:22:19 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+
+        for(int stone : stones){
+            pq.push(stone);
+        }
+
+        while(pq.size() > 1){
+            int first = pq.top();
+            pq.pop();
+
+            int second = pq.top();
+            pq.pop();
+
+            int remaining = first - second;
+
+            if(remaining > 0)
+                pq.push(remaining);
+        }
+
+        if(!pq.empty())
+            return pq.top();
+
+        return 0;
+    }
+};
